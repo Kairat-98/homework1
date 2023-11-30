c43aca56fa60988066a0c06daba26de2aaadcfa0
Kairat Talantbekov <aytikeevkairat@gmail.com>
2023-11-30 19:56:12 +0600

    finished homework 1-2

--- js/home_works.js
+++ js/home_works.js
@@ -15,18 +15,120 @@ gmailButton.addEventListener('click', () => {
     }
 })
 
+
 const childBlock = document.querySelector('.child_block');
 let leftBox = 0;
-const boxFuction = () => {
-  
+let topBox = 0;
+let animationInterval = 16;
+
+const boxFunction = () => {
     setTimeout(() => {
         if (leftBox < 449) {
             childBlock.style.left = `${leftBox}px`;
-            leftBox++
-            boxFuction()
-        } 
-    },2)
-    
+            leftBox++;
+            boxFunction();
+        } else {
+            boxDownFunction();
+        }
+    }, animationInterval);
+};
+
+const boxDownFunction = () => {
+    setTimeout(() => {
+        if (topBox < 449) {
+            childBlock.style.top = `${topBox}px`;
+            topBox++;
+            boxDownFunction();
+        } else {
+            boxRightFunction();
+        }
+    }, animationInterval);
+};
+
+const boxRightFunction = () => {
+    setTimeout(() => {
+        if (leftBox > 0) {
+            childBlock.style.left = `${leftBox}px`;
+            leftBox--;
+            boxRightFunction();
+        } else {
+            boxUpFunction();
+        }
+    }, animationInterval);
+};
+
+const boxUpFunction = () => {
+    setTimeout(() => {
+        if (topBox > 0) {
+            childBlock.style.top = `${topBox}px`;
+            topBox--;
+            boxUpFunction();
+        } else {
+        }
+    }, animationInterval);
+};
+
+boxFunction();
+
+
+// homework 2
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let timer;
+
+const minutesElement = document.getElementById("minutesS");
+const secondsElement = document.getElementById("secondsS");
+const millisecondsElement = document.getElementById("ml-secondsS");
+
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+
+function startTimer() {
+    timer = setInterval(updateTimer, 10);
 }
 
-boxFuction()
+function stopTimer() {
+    clearInterval(timer);
+}
+
+function resetTimer() {
+    stopTimer();
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+    updateDisplay();
+}
+
+function updateTimer() {
+    milliseconds += 10;
+
+    if (milliseconds === 1000) {
+        milliseconds = 0;
+        seconds++;
+
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+    }
+
+    updateDisplay();
+}
+
+function updateDisplay() {
+    minutesElement.textContent = padNumber(minutes);
+    secondsElement.textContent = padNumber(seconds);
+    millisecondsElement.textContent = padNumber(milliseconds);
+}
+
+function padNumber(number) {
+    return number.toString().padStart(2, "0");
+}
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+resetButton.addEventListener("click", resetTimer);
+
+
